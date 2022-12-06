11dd69ebad576ba375c819e447379a1cd92ab71d
raian26 <raian.guilherme@gmail.com>
2022-12-06 17:51:28 -0300

    urna finalizada

---
+++ projeto-urna/etapas.js
@@ -0,0 +1,48 @@
+let etapas =[
+    {
+        titulo:'SENADOR',
+        numeros: 3,
+        candidatos: [{
+            numero: '500',
+            nome: 'Ze do bar',
+            partido: 'PSOL',
+            fotos:[
+                {url:'camarada_marx.jpg' , legenda: 'SENADOR'}
+            ]
+        },
+        { 
+            numero: '100',
+            nome: 'Astronauta anticiência',
+            partido: 'PL',
+            fotos:[
+                {url:'marcos.jpg' , legenda: 'SENADOR'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo:'PRESIDENTE',
+        numeros:2,
+        candidatos:[{
+            numero:'13',
+            nome: 'Lula',
+            partido: 'PT',
+            fotos:[
+                {url: 'lula.jpg' , legenda: 'PRESIDENTE'},
+                {url: 'camarada_alck.jpg' , legenda: 'VICE-PRESIDENTE'}
+            ]
+        },
+        {
+                numero:'22',
+                nome: 'Broxonaro',
+                partido: 'PL',
+                fotos:[
+                    {url: 'bozo.jpg' , legenda: 'PRESIDENTE'},
+                    {url: 'general.jpg' , legenda: 'VICE-PRESIDENTE'}       
+        ]
+    }
+]
+},
+];
+
+    
